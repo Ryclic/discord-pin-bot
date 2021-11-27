1513faca153ea83ae975e572fe2efc0eae3919a4
Ryan Chen <ryanchen175@gmail.com>
2021-11-26 22:45:05 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_SRV, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Successfully connected to the database!');
+})
+.catch((err) => {
+    console.log(err);
+})
