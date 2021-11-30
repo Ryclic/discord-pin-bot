a3b83250b11acea4f415064136e9dc21f60c35d9
Ryan Chen <ryanchen175@gmail.com>
2021-11-29 19:03:19 -0800

    rename main file

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Client, Intents } = require('discord.js');
+const mongoose = require('mongoose');
+require('dotenv').config();
+// discord intents
+const client = new Client({ 
+    intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES
+    ]
+ })
+// connect to mongodb
+mongoose.connect(process.env.MONGODB_SRV, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Successfully connected to the database!');
+})
+.catch((err) => {
+    console.log(err);
+})
+// connect to discord
+client.on('ready', () => {
+    console.log('Bot is ready.');
+})
+
+client.on('messageCreate', (message) => {
+    if(message.content == 'Ryan!'){
+        message.reply({
+            content: 'Hi Ryan!',
+        })
+    }
+})
+client.login(process.env.DISCORD_TOKEN);
