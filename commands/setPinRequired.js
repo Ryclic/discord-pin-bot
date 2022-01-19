fe413dbbb02518f8a70f8af42eab22762f07cabf
Ryan Chen <ryanchen175@gmail.com>
2022-01-18 20:36:15 -0800

    formatting

--- commands/setPinRequired.js
+++ commands/setPinRequired.js
@@ -12,15 +12,17 @@ module.exports = {
                 .setRequired(true)),
 
 	async execute(interaction) {
-        await new guildSchema();
         const votecount = interaction.options.getInteger('count');
         const data = {serverID: interaction.guildId, votecount: votecount}
-        // check if there is already document for current server, if not create one
-        // if there is document then find and save given votecount
+        /* 
+            check if there is already document for current server, if not create one
+            if there is a document then find and save given votecount
+        */
         mongoose.connection.db.collection('guild').count(async function(err, count) {
             if(err != undefined) console.dir('Error while inserting votecount to database: ' + err);
             
             const doesExist = await guildSchema.exists({ serverID: interaction.guildId });
+
             if(!(doesExist)) {
                 console.log('No found records for current server, adding new record.');
                 guildSchema(data).save();
